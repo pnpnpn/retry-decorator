b985477f215c67dd29c07bf4fb0a3ebd7da2e724
Robert Schweikert <rjschwei@suse.com>
2013-08-12 15:58:36 -0400

    - add Makefile to generate versioned tarball - add setup.py to help with installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'retry-decorator',
+    version = '0.1.0',
+    description = 'Decorator for retrying when exceptions occur',
+    author = 'pnpnpn',
+    packages = ['retry_decorator'],
+    url = 'https://github.com/pnpnpn/retry-decorator',
+    )
